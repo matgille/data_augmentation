700c17f590d40d92020f9ab0c2648679fc921769
Serena Crespi <serena.crespi@univ-tours.fr>
2025-09-25 14:42:45 +0200

    [edit] imported

---
+++ data_generation.py
@@ -0,0 +1,42 @@
+import argparse
+import json
+from preprocessing import polygonExtraction
+from preprocessing import cleaner
+from preprocessing import augmentation
+from scriptlevelaugICFHR2022 import script_aug
+from preprocessing import rebuild_pages
+
+def load_config(path):
+    with open(path) as f:
+        return json.load(f)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='config.json')
+    args, _ = parser.parse_known_args()
+
+    config = load_config(args.config)
+
+    parser.set_defaults(**config)
+    args = parser.parse_args()
+
+    augmentation.augmentation(args)
+
+
+    polygonExtraction.polygon_extraction(args)
+
+    cleaner.clean_output_lines(input_dir=args.binarized_lines,
+                               output_dir=args.binarized_lines_clean,
+                               min_area=args.cleaner_min_area,
+                               preview=False)
+
+    script_aug.generate(args)
+
+    rebuild_pages.rebuild_pages_by_method(base_folder=args.augmented_lines,
+                                          data_folder=args.src,
+                                          output_folder=args.rebuilt_pages,
+                                          augmentations=args.augmentation_times)
+
+if __name__ == "__main__":
+    main()
+
