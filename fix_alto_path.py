87249521b7a7cd647a475a18ea91fbc7e17cf3d4
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2026-02-03 14:36:36 +0100

    Add fi alto path script

---
+++ fix_alto_path.py
@@ -0,0 +1,23 @@
+
+import lxml.etree as ET  # Faster XML parsing
+import glob
+import sys
+
+alto_ns = {"alto": "http://www.loc.gov/standards/alto/ns-v4#"}
+
+
+def main(files):
+	for file in glob.glob(files):
+		basename = file.split("/")[-1].split(".")[0]
+		parsed = ET.parse(file)
+		file_name = parsed.xpath("//alto:sourceImageInformation/alto:fileName/text()", namespaces = alto_ns)[0]
+		print(file_name)
+		print(basename)
+		corresponding_image = f"{basename}.jpg"
+		file_name.tag = corresponding_image
+		with open(file.replace(".xml", ".clean.xml"), "w") as output_file:
+			output_file.write(ET.tostring(parsed).decode())
+
+
+if __name__ == '__main__':
+	main(sys.argv[1])
